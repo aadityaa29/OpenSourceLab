80201b6721676b6382638e1d0e2ac30ed315ae4e
Aditya Pachouri <105430733+aadityaa29@users.noreply.github.com>
2025-07-29 08:06:51 +0000

    Que 1 and Que 2 Solved

---
+++ Lab Assignment 1/que1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter the number of integers: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Number of integers must be positive.\n");
+        return 1;
+    }
+
+    int arr[n];
+    long sum = 0; 
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    double average = (double)sum / n;
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
